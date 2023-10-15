4f97dafaf2c76c0c5b7d829941d050b6b294b869
GP <gladkov@edu.yaconnect.com>
2023-10-15 03:10:30 +0300

    +env

--- skymarket/skymarket/settings.py
+++ skymarket/skymarket/settings.py
@@ -116,9 +116,9 @@ DJOSER = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'diploma',
-        'USER': 'postgres',
-        'PASSWORD': 'Ametist371',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
         'HOST': 'db',
         'PORT': 5432,
 
